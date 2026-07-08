0e07067aafe3e943fa97001b3fa8d7ef061747d9
MdEhathisam <ehathisam67hotmail@gmail.com>
2026-07-08 15:51:09 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,22 +1,18 @@
+import os
 import streamlit as st
-
 from google import genai
 
 api_key = os.environ.get("GEMINI_API_KEY")
 if not api_key:
     st.error("GEMINI_API_KEY not set")
     st.stop()
-  
+
 myaibot = genai.Client(api_key=api_key)
-
 st.title("My Own GPT")
-
 question = st.text_input("Ask Anything")
-
 if st.button("Send"):
-  response = myaibot.models.generate_content(
-  model="gemini-2.5-flash",
-  contents = question
-  )
-
-  st.write(response.text)
+    response = myaibot.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=question
+    )
+    st.write(response.text)
